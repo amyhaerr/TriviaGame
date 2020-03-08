458be21ac0f58555a4b1e396a70b3b6feff19d93
Sangriale <senora.pelos@gmail.com>
2020-03-08 11:59:35 -0500

    began javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function () {
+
+
+    //variables needed to play the game:
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+
+
+
+
+// questions and answers for the game:
+
+var game = [{
+
+    question: "What is Crowley's nickname for Sam?",
+    answer: ["Squirrel", "Moose", "Hedwig", "Jerry"],
+    correct: "2",
+    image: ("assets/images/moose.jpg")
+}; {
+
+}
+
+}];
+
+});
